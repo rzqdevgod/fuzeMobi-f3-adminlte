3303609ca16362c02a7fa3452b594b3ced5cb726
rzqdevgod <rizqi.hamid41@gmail.com>
2025-08-11 14:00:28 +0800

    update sql dump

--- appdb.sql
+++ appdb.sql
@@ -1,34 +1,69 @@
-use appdb;
--- --------------------------------------------------------
--- Host:                         localhost
--- Versi server:                 5.5.25a - MySQL Community Server (GPL)
--- OS Server:                    Win64
--- HeidiSQL Versi:               9.2.0.4947
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 11, 2025 at 07:59 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8*/;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Dumping database structure for appdb
+--
+-- Database: `appdb`
+--
 
+-- --------------------------------------------------------
 
--- Dumping structure for table appdb.accounts
-CREATE TABLE IF NOT EXISTS `accounts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
   `email` varchar(200) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `realname` varchar(100) DEFAULT NULL,
-  `lastlogin` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `lastlogin` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table appdb.accounts: ~0 rows (approximately)
-/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+--
+-- Dumping data for table `accounts`
+--
+
 INSERT INTO `accounts` (`id`, `email`, `password`, `realname`, `lastlogin`) VALUES
-	(2, 'hendro.steven@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'Hendro Steven', '2015-08-22 13:06:21');
-/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+(2, 'hendro.steven@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'Hendro Steven', '2015-08-22 13:06:21'),
+(3, 'test@test.com', '202cb962ac59075b964b07152d234b70', 'test user', '2025-08-10 17:44:04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
